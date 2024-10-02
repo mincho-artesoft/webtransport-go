e0c83b9e512dd38712c55d00a4d0c277d40fd0dd
Aleksandar Iliev Svinarov <47814169+JatosDetking@users.noreply.github.com>
2024-10-02 10:10:18 +0300

    quic-go multiplexer

--- vendor/github.com/quic-go/quic-go/multiplexer.go
+++ vendor/github.com/quic-go/quic-go/multiplexer.go
@@ -20,12 +20,10 @@ type multiplexer interface {
 	RemoveConn(indexableConn) error
 }
 
-// The connMultiplexer listens on multiple net.PacketConns and dispatches
-// incoming packets to the connection handler.
 type connMultiplexer struct {
 	mutex sync.Mutex
 
-	conns  map[string] /* LocalAddr().String() */ indexableConn
+	conns  map[string][]indexableConn // Changed to store a slice of indexableConn
 	logger utils.Logger
 }
 
@@ -34,7 +32,7 @@ var _ multiplexer = &connMultiplexer{}
 func getMultiplexer() multiplexer {
 	connMuxerOnce.Do(func() {
 		connMuxer = &connMultiplexer{
-			conns:  make(map[string]indexableConn),
+			conns:  make(map[string][]indexableConn),
 			logger: utils.DefaultLogger.WithPrefix("muxer"),
 		}
 	})
@@ -50,15 +48,7 @@ func (m *connMultiplexer) AddConn(c indexableConn) {
 	defer m.mutex.Unlock()
 
 	connIndex := m.index(c.LocalAddr())
-	p, ok := m.conns[connIndex]
-	if ok {
-		// Panics if we're already listening on this connection.
-		// This is a safeguard because we're introducing a breaking API change, see
-		// https://github.com/quic-go/quic-go/issues/3727 for details.
-		// We'll remove this at a later time, when most users of the library have made the switch.
-		panic("connection already exists") // TODO: write a nice message
-	}
-	m.conns[connIndex] = p
+	m.conns[connIndex] = append(m.conns[connIndex], c)
 }
 
 func (m *connMultiplexer) RemoveConn(c indexableConn) error {
@@ -66,10 +56,19 @@ func (m *connMultiplexer) RemoveConn(c indexableConn) error {
 	defer m.mutex.Unlock()
 
 	connIndex := m.index(c.LocalAddr())
-	if _, ok := m.conns[connIndex]; !ok {
-		return fmt.Errorf("cannote remove connection, connection is unknown")
+	conns, ok := m.conns[connIndex]
+	if !ok {
+		return fmt.Errorf("cannot remove connection, connection is unknown")
 	}
 
-	delete(m.conns, connIndex)
-	return nil
+	for i, conn := range conns {
+		if conn == c {
+			m.conns[connIndex] = append(conns[:i], conns[i+1:]...)
+			if len(m.conns[connIndex]) == 0 {
+				delete(m.conns, connIndex)
+			}
+			return nil
+		}
+	}
+	return fmt.Errorf("cannot remove connection, connection not found")
 }
